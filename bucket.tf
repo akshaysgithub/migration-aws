8ee652912563044a6ee42468ae1d6321007fa136
Akshay Sulakhe <akshay.sulakhe@real-digital.de>
2025-09-23 14:42:51 +0530

    INFRA-cross-acount-replication

--- bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "buckets" {
-  for_each = toset(local.env.bucket_names)
-
-  bucket = each.key
-}
--- iam.tf
+++ iam.tf
@@ -104,6 +104,81 @@ resource "aws_iam_role_policy_attachment" "terraform_cross_account_role_prod" {
   role       = aws_iam_role.terraform_cross_account_role_prod[0].name
 }
 
+
+############## Gitlab runner part #################
+
+# Create an IAM role for EC2 (ephemeral runners)
+resource "aws_iam_role" "ephemeral_runner_role" {
+  count = local.create_prod_iam ? 1 : 0
+
+  name = "ephemeral-runner-role"
+  path = "/gitlab/"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  max_session_duration = 3600
+  tags = {
+    Purpose     = "GitLab ephemeral runner"
+    Environment = "prod"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Attach AdministratorAccess for now (scope down later)
+resource "aws_iam_role_policy_attachment" "ephemeral_runner_admin" {
+  count = local.create_prod_iam ? 1 : 0
+
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.ephemeral_runner_role[0].name
+}
+
+# Create the instance profile for EC2
+resource "aws_iam_instance_profile" "ephemeral_runner_profile" {
+  count = local.create_prod_iam ? 1 : 0
+
+  name = "ephemeral-runner-profile"
+  path = "/gitlab/"
+  role = aws_iam_role.ephemeral_runner_role[0].name
+}
+
+resource "aws_iam_role_policy" "ephemeral_runner_assume_crossaccount" {
+  count = local.create_prod_iam ? 1 : 0
+
+  name = "AssumeDevStagingCrossAccount"
+  role = aws_iam_role.ephemeral_runner_role[0].name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Resource = [
+          "arn:aws:iam::${var.staging_account_id}:role/terraform/TerraformCrossAccountRole",
+          "arn:aws:iam::${var.dev_account_id}:role/terraform/TerraformCrossAccountRole"
+        ]
+      }
+    ]
+  })
+}
+
+output "ephemeral_runner_instance_profile_arn" {
+  value       = local.create_prod_iam ? aws_iam_instance_profile.ephemeral_runner_profile[0].arn : null
+  description = "Instance profile ARN for ephemeral GitLab runners"
+}
+
+
+################## Outputs ##########################
+
 # Output the access keys (handle securely!) - only when created
 output "prod_service_account_access_key" {
   value = local.create_prod_iam ? aws_iam_access_key.prod_service_account[0].id : null
